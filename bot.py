3b1dfcdf797d66b4d8db08e7871bbaec1d2d8571
arklual <arklual1@gmail.com>
2023-05-31 18:09:19 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,71 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from datetime import datetime, date
+import zoneinfo
+from strings import *
+from settings import *
+
+zone = zoneinfo.ZoneInfo("Europe/Moscow")
+app = Client("bot", api_id=api_id, api_hash=api_hash)
+
+@app.on_message(filters.chat(posts_from_1))
+async def on_new_message_1(client, message:Message):
+    if date.today().weekday() > 3:
+        return
+    if datetime.now(zone).hour < 9 or datetime.now(zone).hour > 12:
+        return
+    text = ''
+    file_id = None
+    if message.text:
+        text = message.text
+    elif message.photo:
+        if message.caption:
+            text = message.caption
+            file_id = message.photo.file_id
+        else:
+            await app.send_photo(send_to_1, message.photo.file_id)
+            return
+
+    if '#старт' in text.lower():
+        await app.send_message(send_to_1, START_MESSAGE)
+    elif '#бо' in text.lower():
+        await app.send_message(send_to_1, BO_MESSAGE)
+    else:
+        if file_id:
+            await app.send_photo(send_to_1, file_id, text)
+        else:
+            await app.send_message(send_to_1, text)
+
+@app.on_message(filters.chat(send_to_2))
+async def on_new_message_2(client, message:Message):
+    print(2)
+    if date.today().weekday() > 4:
+        return
+    if datetime.now(zone).hour < 13 or datetime.now(zone).hour > 22:
+        return
+    text = ''
+    file_id = None
+    if message.text:
+        text = message.text
+    elif message.photo:
+        if message.caption:
+            text = message.caption
+            file_id = message.photo.file_id
+        else:
+            await app.send_photo(send_to_2, message.photo.file_id)
+            return
+    print('Текст:', text)
+    if '#об' in text.lower():
+        await app.send_message(send_to_2, OB_MESSAGE)
+    elif '#трейд' in text.lower():
+        await app.send_message(send_to_2, TRADE_MESSAGE)
+    elif '#стоп' in text.lower():
+        await app.send_message(send_to_2, STOP_MESSAGE)
+    else:
+        if file_id:
+            await app.send_photo(send_to_2, file_id, text)
+        else:
+            await app.send_message(send_to_2, text)
+
+
+app.run()
---
+++ settings.py
@@ -0,0 +1,6 @@
+send_to_1 = 'me'
+send_to_2 = 'me'
+posts_from_1 = 'binarsignal'
+posts_from_2 = -1001280819525
+api_id = '23861591'
+api_hash = 'ad141eb62a26b7ae5f0fc2503dd90c02'
